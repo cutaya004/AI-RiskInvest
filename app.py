84e6ead18c76a807478ac1b5ca8a23b123e7fe00
AYA <aya.elbouhouti@etu.uae.ac.ma>
2026-01-18 00:46:08 +0100

    Implement stock price prediction app with Streamlit
    Added Streamlit app for stock price prediction using a pre-trained model and scaler.

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
+import streamlit as st
+import numpy as np
+import joblib
 
+# تحميل الموديل و scaler
+model = joblib.load("riskinvest_model.pkl")
+scaler = joblib.load("scaler.pkl")
+
+st.title("📈 AI-RiskInvest")
+st.write("Application de prédiction boursière et gestion du risque")
+
+st.subheader("Entrer les 60 derniers prix de clôture")
+
+prices = []
+for i in range(60):
+    price = st.number_input(f"Prix {i+1}", value=0.0)
+    prices.append(price)
+
+if st.button("Prédire"):
+    prices_array = np.array(prices).reshape(-1, 1)
+    prices_scaled = scaler.transform(prices_array)
+    X_input = prices_scaled.reshape(1, -1)
+
+    prediction = model.predict(X_input)
+    predicted_price = scaler.inverse_transform(prediction.reshape(-1,1))[0][0]
+
+    st.success(f"📊 Prix prédit : {predicted_price:.2f}")
